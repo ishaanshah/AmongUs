f5ab429c821fc10149cad16e62626e15b130cda4
Ishaan Shah <ishaan.n.shah@gmail.com>
2021-03-25 21:18:03 +0530

    Add collision logic

--- include/game_object.hpp
+++ include/game_object.hpp
@@ -27,15 +27,16 @@ class GameObject {
                    std::vector<float> Vertices, bool IsSolid);
 
         // Gets bounding box of the object
-        std::vector<glm::vec2> GetBoundBox();
+        std::pair<glm::vec2, glm::vec2> GetBoundBox();
 
         // Move the object, returns a list of objects which have been hit
-        std::vector<GameObject> Move(enum Direction direction,
+        std::vector<GameObject *> Move(enum Direction direction,
                                      const std::vector<GameObject> &hittables,
                                      float velocity);
 
         // Render the object
         void Render();
+        glm::mat4 GetModelMatrix();
     private:
         unsigned int VAO;
         std::vector<float> Vertices;
--- src/game_object.cpp
+++ src/game_object.cpp
@@ -32,31 +32,70 @@ GameObject::GameObject(glm::vec2 Position, glm::vec2 Direction,
     glBindVertexArray(0);
 }
 
-std::vector<glm::vec2> GameObject::GetBoundBox() {
-    // TODO: Complete this function
+std::pair<glm::vec2, glm::vec2> GameObject::GetBoundBox() {
+    float minX = 1e10;
+    float minY = 1e10;
+    float maxX = -1e10;
+    float maxY = -1e10;
+
+    glm::mat4 model = GetModelMatrix();
+    for (int i = 0; i < this->Vertices.size() / 5; i++) {
+        glm::vec4 vertex = glm::vec4(this->Vertices[i*5],  this->Vertices[i*5+1],
+                                     0.0f, 1.0f);
+        vertex = model * vertex;
+        minX = std::min(vertex.x, minX);
+        minY = std::min(vertex.y, minY);
+
+        maxX = std::max(vertex.x, maxX);
+        maxY = std::max(vertex.y, maxY);
+    }
+
+    return std::make_pair(glm::vec2(minX, minY), glm::vec2(maxX-minX, maxY-minY));
 }
 
-std::vector<GameObject> GameObject::Move(enum Direction direction,
+std::vector<GameObject *> GameObject::Move(enum Direction direction,
                                          const std::vector<GameObject> &hittables,
                                          float velocity) {
-    std::vector<GameObject> hits;
+    std::vector<GameObject *> hits;
 
-    glm::vec2 newPosition(this->Position);
+    glm::vec2 oldPosition(this->Position);
     if (direction == LEFT) {
-        newPosition.x -= velocity;
+        this->Position.x -= velocity;
     }
     if (direction == RIGHT) {
-        newPosition.x += velocity;
+        this->Position.x += velocity;
     }
     if (direction == UP) {
-        newPosition.y -= velocity;
+        this->Position.y -= velocity;
     }
     if (direction == DOWN) {
-        newPosition.y += velocity;
+        this->Position.y += velocity;
     }
 
     // TODO: Check collisions
-    this->Position = newPosition;
+    bool commitMove = true;
+    std::pair<glm::vec2, glm::vec2> selfBox = GetBoundBox();
+    for (auto hittable: hittables) {
+        std::pair<glm::vec2, glm::vec2> hittableBox = hittable.GetBoundBox();
+
+        // Collision along X-Axis
+        bool collisionX = (hittableBox.first.x + hittableBox.second.x >= selfBox.first.x && 
+                           selfBox.first.x + selfBox.second.x >= hittableBox.first.x);
+
+        // Collision along Y-Axis
+        bool collisionY = (hittableBox.first.y + hittableBox.second.y >= selfBox.first.y && 
+                           selfBox.first.y + selfBox.second.y >= hittableBox.first.y);
+
+        // Collision only if on both axes
+        bool collision = collisionX && collisionY;
+        if (collision) {
+            hits.push_back(&hittable);
+        }
+        commitMove = commitMove && !(collision && hittable.IsSolid);
+    }
+    if (!commitMove) {
+        this->Position = oldPosition;
+    }
 
     return hits;
 }
@@ -67,8 +106,7 @@ void GameObject::Render() {
     Shader shader = ResourceManager::GetShader("object").Use();
 
     // Get transation matrix
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(Position, 0.0f));
+    glm::mat4 model = GetModelMatrix();
 
     // TODO: Figure out flip
     shader.SetMatrix4("model", model);
@@ -76,4 +114,12 @@ void GameObject::Render() {
     glBindVertexArray(this->VAO);
     glDrawArrays(GL_TRIANGLES, 0, this->Vertices.size() / 5);
     glBindVertexArray(0);
+}
+
+glm::mat4 GameObject::GetModelMatrix() {
+    // Get translation matrix
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(this->Position, 0.0f));
+
+    return model;
 }
